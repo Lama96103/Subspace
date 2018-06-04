3776543fc26ff46e7da8a72b91c5c7b4f9f7f74c
Lars Wunderlich <lars.wunderlich@education-siemens.com>
2018-06-04 20:55:43 +0200

    Clean up
    Cleaned Up, Deleted Unity Tutorial Messages, Added point light to ship and locked rotation

---
+++ Subspace/Assets/Scripts/PlayerSetup.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+
+public class PlayerSetup : NetworkBehaviour
+{
+
+    [SerializeField]
+    Behaviour[] componentsToDisable;
+
+    Camera sceneCamera;
+
+    void Start()
+    {
+        if (!isLocalPlayer)
+        {
+            for (int i = 0; i < componentsToDisable.Length; i++)
+            {
+                componentsToDisable[i].enabled = false;
+            }
+        }
+        else
+        {
+            sceneCamera = Camera.main;
+            if (sceneCamera != null)
+            {
+                sceneCamera.gameObject.SetActive(false);
+            }
+            GetComponentInChildren<Camera>().gameObject.name = "PlayerCamera " + Random.Range(0, 10000);
+        }
+    }
+
+    private void OnDisable()
+    {
+        if (sceneCamera != null)
+        {
+            sceneCamera.gameObject.SetActive(true);
+        }
+    }
+
+    public void AfterDead()
+    {
+        if (!isLocalPlayer)
+        {
+            for (int i = 0; i < componentsToDisable.Length; i++)
+            {
+                componentsToDisable[i].enabled = false;
+            }
+        }
+    }
+}
